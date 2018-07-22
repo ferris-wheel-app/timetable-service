f9026fffd7cac97689f7e7fe3751ffb42e32fff7
Mesfin Mebrate <mesfin.mebrate@basementcrowd.com>
2018-07-22 20:01:59 +0100

    Creating the models.

--- src/main/scala/com.ferris.timetable/model/Model.scala
+++ src/main/scala/com.ferris.timetable/model/Model.scala
@@ -1,5 +1,6 @@
 package com.ferris.timetable.model
 
+import java.time.{LocalDate, LocalDateTime}
 import java.util.UUID
 
 object Model {
@@ -8,5 +9,30 @@ object Model {
     uuid: UUID,
     sender: String,
     content: String
+  )
+
+  sealed trait TimeBlock {
+    def start: LocalDateTime
+    def finish: LocalDateTime
+  }
+
+  case class ConcreteBlock (
+    uuid: UUID,
+    start: LocalDateTime,
+    finish: LocalDateTime,
+    task: Option[UUID]
+  ) extends TimeBlock
+
+  case class BufferBlock (
+    uuid: UUID,
+    start: LocalDateTime,
+    finish: LocalDateTime,
+    firstTask: Option[UUID],
+    secondTask: Option[UUID]
+  ) extends TimeBlock
+
+  case class Timetable (
+    date: LocalDate,
+    blocks: Seq[TimeBlock]
   )
 }
