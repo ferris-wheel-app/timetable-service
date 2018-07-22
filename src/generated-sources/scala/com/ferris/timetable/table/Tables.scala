c6869228e4c08f8ac110c676b452acc5f451c227
Mesfin Mebrate <mesfin.mebrate@basementcrowd.com>
2018-07-22 17:00:22 +0100

    Creating the tables.

--- src/generated-sources/scala/com/ferris/timetable/table/Tables.scala
+++ src/generated-sources/scala/com/ferris/timetable/table/Tables.scala
@@ -14,7 +14,7 @@ trait Tables {
   import slick.jdbc.{GetResult => GR}
 
   /** DDL for all tables. Call .create to execute. */
-  lazy val schema: profile.SchemaDescription = MessageTable.schema
+  lazy val schema: profile.SchemaDescription = MessageTable.schema ++ RoutineTable.schema ++ RoutineTimeBlockTable.schema ++ ScheduledTimeBlockTable.schema ++ TimeBlockTable.schema
   @deprecated("Use .schema instead of .ddl", "3.0")
   def ddl = schema
 
@@ -49,4 +49,149 @@ trait Tables {
   }
   /** Collection-like TableQuery object for table MessageTable */
   lazy val MessageTable = new TableQuery(tag => new MessageTable(tag))
+
+  /** Entity class storing rows of table RoutineTable
+   *  @param id Database column ID SqlType(BIGINT), AutoInc, PrimaryKey
+   *  @param uuid Database column UUID SqlType(VARCHAR), Length(36,true)
+   *  @param name Database column NAME SqlType(VARCHAR), Length(256,true)
+   *  @param isCurrent Database column IS_CURRENT SqlType(TINYINT) */
+  case class RoutineRow(id: Long, uuid: String, name: String, isCurrent: Byte)
+  /** GetResult implicit for fetching RoutineRow objects using plain SQL queries */
+  implicit def GetResultRoutineRow(implicit e0: GR[Long], e1: GR[String], e2: GR[Byte]): GR[RoutineRow] = GR{
+    prs => import prs._
+    RoutineRow.tupled((<<[Long], <<[String], <<[String], <<[Byte]))
+  }
+  /** Table description of table ROUTINE. Objects of this class serve as prototypes for rows in queries. */
+  class RoutineTable(_tableTag: Tag) extends profile.api.Table[RoutineRow](_tableTag, "ROUTINE") {
+    def * = (id, uuid, name, isCurrent) <> (RoutineRow.tupled, RoutineRow.unapply)
+    /** Maps whole row to an option. Useful for outer joins. */
+    def ? = (Rep.Some(id), Rep.Some(uuid), Rep.Some(name), Rep.Some(isCurrent)).shaped.<>({r=>import r._; _1.map(_=> RoutineRow.tupled((_1.get, _2.get, _3.get, _4.get)))}, (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
+
+    /** Database column ID SqlType(BIGINT), AutoInc, PrimaryKey */
+    val id: Rep[Long] = column[Long]("ID", O.AutoInc, O.PrimaryKey)
+    /** Database column UUID SqlType(VARCHAR), Length(36,true) */
+    val uuid: Rep[String] = column[String]("UUID", O.Length(36,varying=true))
+    /** Database column NAME SqlType(VARCHAR), Length(256,true) */
+    val name: Rep[String] = column[String]("NAME", O.Length(256,varying=true))
+    /** Database column IS_CURRENT SqlType(TINYINT) */
+    val isCurrent: Rep[Byte] = column[Byte]("IS_CURRENT")
+
+    /** Uniqueness Index over (uuid) (database name CONSTRAINT_INDEX_7) */
+    val index1 = index("CONSTRAINT_INDEX_7", uuid, unique=true)
+  }
+  /** Collection-like TableQuery object for table RoutineTable */
+  lazy val RoutineTable = new TableQuery(tag => new RoutineTable(tag))
+
+  /** Entity class storing rows of table RoutineTimeBlockTable
+   *  @param id Database column ID SqlType(BIGINT), AutoInc, PrimaryKey
+   *  @param routineId Database column ROUTINE_ID SqlType(BIGINT)
+   *  @param timeBlockId Database column TIME_BLOCK_ID SqlType(BIGINT)
+   *  @param dayOfWeek Database column DAY_OF_WEEK SqlType(VARCHAR), Length(36,true) */
+  case class RoutineTimeBlockRow(id: Long, routineId: Long, timeBlockId: Long, dayOfWeek: String)
+  /** GetResult implicit for fetching RoutineTimeBlockRow objects using plain SQL queries */
+  implicit def GetResultRoutineTimeBlockRow(implicit e0: GR[Long], e1: GR[String]): GR[RoutineTimeBlockRow] = GR{
+    prs => import prs._
+    RoutineTimeBlockRow.tupled((<<[Long], <<[Long], <<[Long], <<[String]))
+  }
+  /** Table description of table ROUTINE_TIME_BLOCK. Objects of this class serve as prototypes for rows in queries. */
+  class RoutineTimeBlockTable(_tableTag: Tag) extends profile.api.Table[RoutineTimeBlockRow](_tableTag, "ROUTINE_TIME_BLOCK") {
+    def * = (id, routineId, timeBlockId, dayOfWeek) <> (RoutineTimeBlockRow.tupled, RoutineTimeBlockRow.unapply)
+    /** Maps whole row to an option. Useful for outer joins. */
+    def ? = (Rep.Some(id), Rep.Some(routineId), Rep.Some(timeBlockId), Rep.Some(dayOfWeek)).shaped.<>({r=>import r._; _1.map(_=> RoutineTimeBlockRow.tupled((_1.get, _2.get, _3.get, _4.get)))}, (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
+
+    /** Database column ID SqlType(BIGINT), AutoInc, PrimaryKey */
+    val id: Rep[Long] = column[Long]("ID", O.AutoInc, O.PrimaryKey)
+    /** Database column ROUTINE_ID SqlType(BIGINT) */
+    val routineId: Rep[Long] = column[Long]("ROUTINE_ID")
+    /** Database column TIME_BLOCK_ID SqlType(BIGINT) */
+    val timeBlockId: Rep[Long] = column[Long]("TIME_BLOCK_ID")
+    /** Database column DAY_OF_WEEK SqlType(VARCHAR), Length(36,true) */
+    val dayOfWeek: Rep[String] = column[String]("DAY_OF_WEEK", O.Length(36,varying=true))
+
+    /** Foreign key referencing RoutineTable (database name ROUTINE_FK) */
+    lazy val routineTableFk = foreignKey("ROUTINE_FK", routineId, RoutineTable)(r => r.id, onUpdate=ForeignKeyAction.Restrict, onDelete=ForeignKeyAction.Restrict)
+    /** Foreign key referencing TimeBlockTable (database name TIME_BLOCK_FK_1) */
+    lazy val timeBlockTableFk = foreignKey("TIME_BLOCK_FK_1", timeBlockId, TimeBlockTable)(r => r.id, onUpdate=ForeignKeyAction.Restrict, onDelete=ForeignKeyAction.Restrict)
+  }
+  /** Collection-like TableQuery object for table RoutineTimeBlockTable */
+  lazy val RoutineTimeBlockTable = new TableQuery(tag => new RoutineTimeBlockTable(tag))
+
+  /** Entity class storing rows of table ScheduledTimeBlockTable
+   *  @param id Database column ID SqlType(BIGINT), AutoInc, PrimaryKey
+   *  @param uuid Database column UUID SqlType(VARCHAR), Length(36,true)
+   *  @param date Database column DATE SqlType(DATE)
+   *  @param timeBlockId Database column TIME_BLOCK_ID SqlType(BIGINT)
+   *  @param temporalStatus Database column TEMPORAL_STATUS SqlType(VARCHAR), Length(36,true)
+   *  @param isDone Database column IS_DONE SqlType(TINYINT), Default(0) */
+  case class ScheduledTimeBlockRow(id: Long, uuid: String, date: java.sql.Date, timeBlockId: Long, temporalStatus: String, isDone: Byte = 0)
+  /** GetResult implicit for fetching ScheduledTimeBlockRow objects using plain SQL queries */
+  implicit def GetResultScheduledTimeBlockRow(implicit e0: GR[Long], e1: GR[String], e2: GR[java.sql.Date], e3: GR[Byte]): GR[ScheduledTimeBlockRow] = GR{
+    prs => import prs._
+    ScheduledTimeBlockRow.tupled((<<[Long], <<[String], <<[java.sql.Date], <<[Long], <<[String], <<[Byte]))
+  }
+  /** Table description of table SCHEDULED_TIME_BLOCK. Objects of this class serve as prototypes for rows in queries. */
+  class ScheduledTimeBlockTable(_tableTag: Tag) extends profile.api.Table[ScheduledTimeBlockRow](_tableTag, "SCHEDULED_TIME_BLOCK") {
+    def * = (id, uuid, date, timeBlockId, temporalStatus, isDone) <> (ScheduledTimeBlockRow.tupled, ScheduledTimeBlockRow.unapply)
+    /** Maps whole row to an option. Useful for outer joins. */
+    def ? = (Rep.Some(id), Rep.Some(uuid), Rep.Some(date), Rep.Some(timeBlockId), Rep.Some(temporalStatus), Rep.Some(isDone)).shaped.<>({r=>import r._; _1.map(_=> ScheduledTimeBlockRow.tupled((_1.get, _2.get, _3.get, _4.get, _5.get, _6.get)))}, (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
+
+    /** Database column ID SqlType(BIGINT), AutoInc, PrimaryKey */
+    val id: Rep[Long] = column[Long]("ID", O.AutoInc, O.PrimaryKey)
+    /** Database column UUID SqlType(VARCHAR), Length(36,true) */
+    val uuid: Rep[String] = column[String]("UUID", O.Length(36,varying=true))
+    /** Database column DATE SqlType(DATE) */
+    val date: Rep[java.sql.Date] = column[java.sql.Date]("DATE")
+    /** Database column TIME_BLOCK_ID SqlType(BIGINT) */
+    val timeBlockId: Rep[Long] = column[Long]("TIME_BLOCK_ID")
+    /** Database column TEMPORAL_STATUS SqlType(VARCHAR), Length(36,true) */
+    val temporalStatus: Rep[String] = column[String]("TEMPORAL_STATUS", O.Length(36,varying=true))
+    /** Database column IS_DONE SqlType(TINYINT), Default(0) */
+    val isDone: Rep[Byte] = column[Byte]("IS_DONE", O.Default(0))
+
+    /** Foreign key referencing TimeBlockTable (database name TIME_BLOCK_FK_2) */
+    lazy val timeBlockTableFk = foreignKey("TIME_BLOCK_FK_2", timeBlockId, TimeBlockTable)(r => r.id, onUpdate=ForeignKeyAction.Restrict, onDelete=ForeignKeyAction.Restrict)
+
+    /** Uniqueness Index over (uuid) (database name CONSTRAINT_INDEX_E) */
+    val index1 = index("CONSTRAINT_INDEX_E", uuid, unique=true)
+  }
+  /** Collection-like TableQuery object for table ScheduledTimeBlockTable */
+  lazy val ScheduledTimeBlockTable = new TableQuery(tag => new ScheduledTimeBlockTable(tag))
+
+  /** Entity class storing rows of table TimeBlockTable
+   *  @param id Database column ID SqlType(BIGINT), AutoInc, PrimaryKey
+   *  @param uuid Database column UUID SqlType(VARCHAR), Length(36,true)
+   *  @param startTime Database column START_TIME SqlType(TIME)
+   *  @param finishTime Database column FINISH_TIME SqlType(TIME)
+   *  @param taskType Database column TASK_TYPE SqlType(VARCHAR), Length(36,true)
+   *  @param taskId Database column TASK_ID SqlType(VARCHAR), Length(36,true) */
+  case class TimeBlockRow(id: Long, uuid: String, startTime: java.sql.Time, finishTime: java.sql.Time, taskType: String, taskId: Option[String])
+  /** GetResult implicit for fetching TimeBlockRow objects using plain SQL queries */
+  implicit def GetResultTimeBlockRow(implicit e0: GR[Long], e1: GR[String], e2: GR[java.sql.Time], e3: GR[Option[String]]): GR[TimeBlockRow] = GR{
+    prs => import prs._
+    TimeBlockRow.tupled((<<[Long], <<[String], <<[java.sql.Time], <<[java.sql.Time], <<[String], <<?[String]))
+  }
+  /** Table description of table TIME_BLOCK. Objects of this class serve as prototypes for rows in queries. */
+  class TimeBlockTable(_tableTag: Tag) extends profile.api.Table[TimeBlockRow](_tableTag, "TIME_BLOCK") {
+    def * = (id, uuid, startTime, finishTime, taskType, taskId) <> (TimeBlockRow.tupled, TimeBlockRow.unapply)
+    /** Maps whole row to an option. Useful for outer joins. */
+    def ? = (Rep.Some(id), Rep.Some(uuid), Rep.Some(startTime), Rep.Some(finishTime), Rep.Some(taskType), taskId).shaped.<>({r=>import r._; _1.map(_=> TimeBlockRow.tupled((_1.get, _2.get, _3.get, _4.get, _5.get, _6)))}, (_:Any) =>  throw new Exception("Inserting into ? projection not supported."))
+
+    /** Database column ID SqlType(BIGINT), AutoInc, PrimaryKey */
+    val id: Rep[Long] = column[Long]("ID", O.AutoInc, O.PrimaryKey)
+    /** Database column UUID SqlType(VARCHAR), Length(36,true) */
+    val uuid: Rep[String] = column[String]("UUID", O.Length(36,varying=true))
+    /** Database column START_TIME SqlType(TIME) */
+    val startTime: Rep[java.sql.Time] = column[java.sql.Time]("START_TIME")
+    /** Database column FINISH_TIME SqlType(TIME) */
+    val finishTime: Rep[java.sql.Time] = column[java.sql.Time]("FINISH_TIME")
+    /** Database column TASK_TYPE SqlType(VARCHAR), Length(36,true) */
+    val taskType: Rep[String] = column[String]("TASK_TYPE", O.Length(36,varying=true))
+    /** Database column TASK_ID SqlType(VARCHAR), Length(36,true) */
+    val taskId: Rep[Option[String]] = column[Option[String]]("TASK_ID", O.Length(36,varying=true))
+
+    /** Uniqueness Index over (uuid) (database name CONSTRAINT_INDEX_9) */
+    val index1 = index("CONSTRAINT_INDEX_9", uuid, unique=true)
+  }
+  /** Collection-like TableQuery object for table TimeBlockTable */
+  lazy val TimeBlockTable = new TableQuery(tag => new TimeBlockTable(tag))
 }
--- src/main/resources/db/migration/V1__starting_schema.sql
+++ src/main/resources/db/migration/V1__starting_schema.sql
@@ -6,3 +6,45 @@ create table message (
   PRIMARY KEY (id),
   UNIQUE KEY (uuid)
 ) ENGINE=InnoDB;
+
+create table routine (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  uuid VARCHAR(36) NOT NULL,
+  name VARCHAR(256) NOT NULL,
+  is_current TINYINT(1) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY (uuid)
+) ENGINE=InnoDB;
+
+create table time_block (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  uuid VARCHAR(36) NOT NULL,
+  start_time TIME NOT NULL,
+  finish_time TIME NOT NULL,
+  task_type VARCHAR(36) NOT NULL check (task_type in ('THREAD', 'WEAVE', 'LASER_DONUT', 'HOBBY')),
+  task_id VARCHAR(36),
+  PRIMARY KEY (id),
+  UNIQUE KEY (uuid)
+) ENGINE=InnoDB;
+
+create table routine_time_block (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  routine_id BIGINT NOT NULL,
+  time_block_id BIGINT NOT NULL,
+  day_of_week VARCHAR(36) NOT NULL check (day_of_week in ('MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY')),
+  PRIMARY KEY (id),
+  CONSTRAINT routine_fk FOREIGN KEY (routine_id) REFERENCES routine (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT time_block_fk_1 FOREIGN KEY (time_block_id) REFERENCES time_block (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB;
+
+create table scheduled_time_block (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  uuid VARCHAR(36) NOT NULL,
+  date DATE NOT NULL,
+  time_block_id BIGINT NOT NULL,
+  temporal_status VARCHAR(36) NOT NULL check (temporal_status in ('PREVIOUSLY', 'RIGHT_NOW', 'UPCOMING')),
+  is_done TINYINT(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (id),
+  UNIQUE KEY (uuid),
+  CONSTRAINT time_block_fk_2 FOREIGN KEY (time_block_id) REFERENCES time_block (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB;
