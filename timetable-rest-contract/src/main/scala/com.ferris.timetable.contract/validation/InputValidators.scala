f1a38129e24d623c7871d23b60bc986c3b39ec7b
Mesfin Mebrate <mesfin.mebrate@basementcrowd.com>
2018-09-06 21:15:27 +0100

    Implementing the input validation.

--- timetable-rest-contract/src/main/scala/com.ferris.timetable.contract/validation/InputValidators.scala
+++ timetable-rest-contract/src/main/scala/com.ferris.timetable.contract/validation/InputValidators.scala
@@ -1,11 +1,15 @@
 package com.ferris.timetable.contract.validation
 
+import java.time.Duration
+
 import com.ferris.microservice.validation.InputValidation
 import com.ferris.timetable.contract.resource.Resources.In._
 
 object InputValidators extends InputValidation {
 
   private val TypeField = "type"
+
+  private val WholeDay = 86400L
 
   // Check that the time-blocks are in order.
   // Check that there are no gaps and overlaps between time-blocks.
@@ -26,9 +30,18 @@ object InputValidators extends InputValidation {
   }
 
   private def blocksAreChained(creation: TimetableTemplateCreation): Boolean = {
-    val slidingPairs = creation.blocks.sliding(2, 1).collect { case Seq(a, b) => (a, b) }
-    slidingPairs.foldLeft(true) { case (soFar, (previous, next)) =>
+    slidingPairs(creation.blocks).foldLeft(true) { case (soFar, (previous, next)) =>
       soFar && (previous.finish == next.start)
     }
+  }
+
+  private def occupiesWholeDay(creation: TimetableTemplateCreation): Boolean = {
+    creation.blocks.foldLeft(0L) { case (occupiedPeriod, block) =>
+      occupiedPeriod + Duration.between(block.start, block.finish).getSeconds
+    } == WholeDay
+  }
+
+  private def slidingPairs[T](list: Seq[T]): Seq[(T, T)] = {
+    list.sliding(2, 1).collect { case Seq(a, b) => (a, b) }.toSeq
   }
 }
